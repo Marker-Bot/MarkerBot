683364a6057aba19a208886524da6e6a2f7fa95c
Arilon <egorreal2011@gmail.com>
2021-06-09 12:35:56 +0300

    Board detection commented.

--- camera_vision/board_detection.py
+++ camera_vision/board_detection.py
@@ -1,17 +1,64 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 import numpy as np
-from collections import defaultdict
 
 
-def find_board(img, test=False, lenght=700, min_size=1300, centroid=True):
+def nothing(x):
+    """
+    Функция для теста
+
+    :param x: -
+    :return: -
+    """
+    pass
+
+
+def find_board(img, test=False, lenght=883, min_size=527132, centroid=True):
+    """
+
+    :param img: RGB изображение np.array
+    :param test: bool настройка параметров
+    :param lenght: int Минимальная длинна линии доски
+    :param min_size: int Минимальный размер определившейся доски
+    :param centroid: bool Центрирована ли доска на frame
+
+    :return: img, flag - img - перспективное преобразование доски ( кроп доски)
+             flag - True - преобразование выполнено; False - преобразование не выполнено, img - оригинальная картинка
+    """
+    if not test:
+        th_can_down = 60
+        th_can_up = 69
+        th_houg = 135
+        min_leght = lenght
+        gap = 128
+        min_size = min_size
+    else:
+        th_can_down = cv.getTrackbarPos('th_can_down', 'settings')
+        th_can_up = cv.getTrackbarPos('th_can_up', 'settings')
+        th_houg = cv.getTrackbarPos('th_hough', 'settings')
+        min_leght = cv.getTrackbarPos('min_line', 'settings')
+        gap = cv.getTrackbarPos('gap', 'settings')
+        min_size = cv.getTrackbarPos('min_size', 'settings')
+
     b_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    thresh = cv.Canny(b_img, 0, 100, apertureSize=3)
+    thresh = cv.Canny(b_img, th_can_down, th_can_up, apertureSize=3)
     kernel = np.ones((3, 3), np.uint8)
-    thresh = cv.dilate(thresh, kernel, iterations=3)
+    thresh = cv.dilate(thresh, kernel, iterations=4)
 
-    lines = cv.HoughLinesP(thresh, 1, np.pi / 180, 100, minLineLength=lenght, maxLineGap=30)
+    if test:
+        resize = cv.resize(thresh, (480, 480), interpolation=cv.INTER_AREA)
+        resize1 = cv.resize(img, (480, 480), interpolation=cv.INTER_AREA)
+        cv.imshow('img_r', resize1)
+        cv.imshow('canny', resize)
+        k = cv.waitKey(1)
+        if k == ord('q'):
+            cv.destroyAllWindows()
+
+    lines = cv.HoughLinesP(thresh, 1, np.pi / 180, th_houg, minLineLength=min_leght, maxLineGap=gap)
     transform = np.zeros(img.shape, dtype=np.uint8)
+
+    if lines is None:
+        return img, False
 
     for line in lines:
         x1, y1, x2, y2 = line[0]
@@ -20,7 +67,7 @@ def find_board(img, test=False, lenght=700, min_size=1300, centroid=True):
         cv.line(transform, (x1, y1), (x2, y2), (255, 255, 255), 2)
 
     transform = cv.cvtColor(transform, cv.COLOR_BGR2GRAY)
-    transform = cv.dilate(transform, kernel, iterations=7)
+    transform = cv.dilate(transform, kernel, iterations=10)
     transform = cv.bitwise_not(transform)
 
     counters, heir = cv.findContours(transform, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
@@ -36,7 +83,14 @@ def find_board(img, test=False, lenght=700, min_size=1300, centroid=True):
             cy = int(M['m01'] / M['m00'])
             squares.append((cnt, (cx, cy)))
 
+    if len(squares) == 0:
+        return img, False
+
     nearest_square = squares[0]
+
+    if test:
+        print(cv.contourArea(nearest_square[0]))
+
     if centroid:
         img_center = (img.shape[0] // 2, img.shape[1] // 2)
         for square in squares:
@@ -46,9 +100,10 @@ def find_board(img, test=False, lenght=700, min_size=1300, centroid=True):
 
     peri = cv.arcLength(nearest_square[0], True)
     approx = cv.approxPolyDP(nearest_square[0], 0.04 * peri, True)
+
     if test:
         for a in approx:
-            cv.circle(img, (a[0][0], a[0][1]), 5, (255, 0, 0), -1)
+            cv.circle(img, (a[0][0], a[0][1]), 15, (255, 0, 0), -1)
 
     pts1 = np.float32([approx[0][0], approx[1][0], approx[2][0], approx[3][0]])
     pts2 = np.float32([[0, 0], [0, 800], [800, 800], [800, 0]])
@@ -56,21 +111,45 @@ def find_board(img, test=False, lenght=700, min_size=1300, centroid=True):
     M = cv.getPerspectiveTransform(pts1, pts2)
 
     perspective = cv.warpPerspective(img, M, (800, 800))
+
     if test:
         cv.imshow('perspective', perspective)
-        resize = cv.resize(thresh, (480, 480), interpolation=cv.INTER_AREA)
         resize2 = cv.resize(transform, (480, 480), interpolation=cv.INTER_AREA)
-        resize1 = cv.resize(img, (480, 480), interpolation=cv.INTER_AREA)
-        cv.imshow('img_r', resize1)
-        cv.imshow('img', resize)
         cv.imshow('trans', resize2)
-        k = cv.waitKey(0)
+        k = cv.waitKey(1)
         if k == ord('q'):
             cv.destroyAllWindows()
 
-    return perspective
+    return perspective, True
 
 
 if __name__ == "__main__":
-    img = cv.imread(r"/Users/valeriy/Downloads/test_spo.JPG")
-    find_board(img, test=True)
+    # img = cv.imread(r"C:\Users\Arilon\Desktop\Projects\MarkerBot\test_photo_4.png")
+    # find_board(img, test=True)
+
+    test = True
+    cap = cv.VideoCapture(r"C:\Users\Arilon\Desktop\Projects\MarkerBot\1.mp4")
+    if cap.isOpened() is False:
+        print("Wrong")
+        exit()
+
+    if test:
+        cv.namedWindow("result")
+        cv.namedWindow("settings")
+        cv.createTrackbar('th_hough', 'settings', 0, 255, nothing)
+        cv.createTrackbar('min_line', 'settings', 0, 4000, nothing)
+        cv.createTrackbar('gap', 'settings', 0, 500, nothing)
+        cv.createTrackbar('th_can_down', 'settings', 0, 255, nothing)
+        cv.createTrackbar('th_can_up', 'settings', 0, 255, nothing)
+        cv.createTrackbar('min_size', 'settings', 100000, 1000000, nothing)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print('not')
+            continue
+        img, flag = find_board(frame, test=True)
+        cv.imshow("res_img", img)
+        if cv.waitKey(1) == ord("q"):
+            cap.release()
+            exit()
--- camera_vision/robot_detection.py
+++ camera_vision/robot_detection.py
@@ -8,20 +8,26 @@ from camera_vision.board_detection import find_board
 
 # Подобранные трешхолды
 H_MIN_UP = np.array((74, 32, 0), np.uint8)
-H_MAX_UP = np.array((255, 255, 18), np.uint8)
+H_MAX_UP = np.array((255, 255, 130), np.uint8)
 H_MIN_DOWN = np.array((42, 0, 0), np.uint8)
 H_MAX_DOWN = np.array((69, 255, 30), np.uint8)
 
 
 def nothing(x):
+    """
+    Функция для теста
+
+    :param x: -
+    :return: -
+    """
     pass
 
 
 def tuning_color_filter(test_img):
     """
-    Вспомогательная функция для настройки порога цвета метки
+    Вспомогательная функция для настройки порога цвета метки.
 
-    :param test_img: ndarray - image
+    :param test_img: np.array - image
     :return:
     """
     cv.namedWindow("result")
@@ -60,10 +66,12 @@ def tuning_color_filter(test_img):
 def detect_label(img, h_min, h_max):
     """
     Детектирование метки на изображении вычисление по порогу цвета и основным моментам изображения.
-    :param img: ndarray image
-    :param h_min: (h, s, v) tuple of min HSV
-    :param h_max: (h, s, v) tuple of max HSV
-    :return: (x, y) - label's center coords
+
+    :param img: np.array image
+    :param h_min: (h, s, v) кортеж мин. HSV трешхолдов
+    :param h_max: (h, s, v) кортеж макс. HSV трешхолдов
+
+    :return: (x, y) - Координаты центра метки
     """
 
     # To hsv
@@ -87,6 +95,7 @@ def detect_label(img, h_min, h_max):
         x = int(d_m10 / d_area)
         y = int(d_m01 / d_area)
         return (x, y)
+
     else:
         return None
 
@@ -94,11 +103,12 @@ def detect_label(img, h_min, h_max):
 def detect_robot_coords(img, resize=False):
     """
     Детектирование координат робота по фотографии и меткам.
-    :param img: ndarray image
+
+    :param img: np.array image
     :return: (x, y, z) координаты (x, y) ценра , поворот робота; img
     """
 
-    cropped = find_board(img)  # TODO Добавить кроп доски после написания детектора доски
+    cropped = find_board(img)
     if resize:
         # Уменьшаем картинку чтоб было видно на экране
         scale_percent = 20
@@ -117,49 +127,40 @@ def detect_robot_coords(img, resize=False):
     cv.circle(resized, down_label, 5, (255, 0, 0), -1)
     cv.arrowedLine(resized, down_label, up_label, (0, 0, 255), 3)
     vector = (up_label[0] - down_label[0], up_label[1] - down_label[1])
-    angle = vector[0] / (
-        math.sqrt(vector[0] ** 2 + vector[1] ** 2))
+    angle = vector[0] / (math.sqrt(vector[0] ** 2 + vector[1] ** 2))
 
     return up_label, resized, angle
 
 
 if __name__ == '__main__':
-    # test = cv.imread(r'/Users/valeriy/Downloads/test_spo.JPG')
+    # test = cv.imread(r"C:\Users\Arilon\Desktop\Projects\MarkerBot\test_photo_4.png")
+    # cropped = find_board(test)
+    #
+    # _, thresh = cv.threshold(cropped, 127, 253, cv.THRESH_BINARY_INV)
+    # cv.imshow("dkdk", thresh)
+    # if cv.waitKey(0) == ord("q"):
+    #     exit()
+    #
+    # tuning_color_filter(cropped)
     # detect_robot_coords(test)
+    # exit()
 
     cap = cv.VideoCapture(0)
-    # cv.namedWindow("result")
-    # cv.namedWindow("settings")
-    # cv.createTrackbar('h1', 'settings', 0, 255, nothing)
-    # cv.createTrackbar('s1', 'settings', 0, 255, nothing)
-    # cv.createTrackbar('v1', 'settings', 0, 255, nothing)
-    # cv.createTrackbar('h2', 'settings', 255, 255, nothing)
-    # cv.createTrackbar('s2', 'settings', 255, 255, nothing)
-    # cv.createTrackbar('v2', 'settings', 255, 255, nothing)
+
     while True:
         ret, frame = cap.read()
         if not ret:
             print('not')
+            continue
         cv.imshow('fr', frame)
-        center, res, angle = detect_robot_coords(frame)
-        cv.imshow('result', res)
-        print(angle)
-        # hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        k = cv.waitKey(1)
+        if k == ord('q'):
+            break
+        # center, res, angle = detect_robot_coords(frame)
+        # cv.imshow('result', res)
+        crop = find_board(frame)
+        cv.imshow("djjd", crop)
 
-        # h1 = cv.getTrackbarPos('h1', 'settings')
-        # s1 = cv.getTrackbarPos('s1', 'settings')
-        # v1 = cv.getTrackbarPos('v1', 'settings')
-        # h2 = cv.getTrackbarPos('h2', 'settings')
-        # s2 = cv.getTrackbarPos('s2', 'settings')
-        # v2 = cv.getTrackbarPos('v2', 'settings')
-        #
-        # h_min = np.array((h1, s1, v1), np.uint8)
-        # h_max = np.array((h2, s2, v2), np.uint8)
-        #
-        # # накладываем фильтр
-        # thresh = cv.inRange(frame, h_min, h_max)
-
-        # cv.imshow('result', thresh)
         k = cv.waitKey(1)
         if k == ord('q'):
             break
