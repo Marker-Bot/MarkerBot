8665bf17c5c9f341de7e686389d01cae1beeb8bc
Ilya Chapyshev <chapa00@inbox.ru>
2021-06-14 22:31:32 +0300

    Комментарии на гуи

--- ROS/cros_ws/src/hrwros_week1/scripts/coordinates_server.py
+++ ROS/cros_ws/src/hrwros_week1/scripts/coordinates_server.py
@@ -4,20 +4,15 @@ import rospy
 import socket
 from hrwros_msgs.msg import Points_arrays
 
+import Client
+
 # Topic callback function.
 def stringListenerCallback(data):
+    Client.sendData(data.x_coordinates, data.y_coordinates)
     rospy.loginfo('X coordinates: %s', data.x_coordinates)
     rospy.loginfo('Y coordinates: %s', data.y_coordinates)
 
 def stringListener():
-    #sock = socket.socket()
-    #sock.connect(('ev3dev.local', 9090))
-    #sock.send(x_coordinates.encode())
-    #sock.send(y_coordinates.encode())
-
-    #ev3_data = sock.recv(1024).decode()
-    #sock.close()
-
 
     rospy.init_node('coordinates_server_node', anonymous = False)
 
