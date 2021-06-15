ade6f011614cec5cd22e08d873f9af2bef395e43
Ilya Chapyshev <chapa00@inbox.ru>
2021-06-15 15:07:23 +0300

    Добавление детектора в рос

--- ROS/cros_ws/src/hrwros_week1/scripts/detector.py
+++ ROS/cros_ws/src/hrwros_week1/scripts/detector.py
@@ -2,10 +2,12 @@
 # Node to subscribe to a string and print the string on terminal.
 import rospy
 from hrwros_msgs.msg import Points_arrays
+import cv2 as cv
+import numpy as np
 
 
 import camera_vision.board_detection as bd
-import camera_vision.robot_detection as rb
+import camera_vision.robot_detection as rd
 import camera_vision.detector as dt
 
 
@@ -17,26 +19,64 @@ def Detector():
 
 
 	#Test arrays
-	traj_x = [1, 1, 1]
-	traj_y = [1, 1, 1]
+	traj_x = []
+	traj_y = []
+
+	cap = cv.VideoCapture(1)
+	cap.set(5, 20)
 
 	while True:
-		rb.detect_robot_coords()
+		# Считывание изображения с камеры
+		a, img = cap.read()
+		sensitivity = 60
+		lower_bound = np.array([0, 0, 255 - sensitivity])
+		upper_bound = np.array([255, sensitivity, 255])
+
+		x, y, mask = bd.find_color(img, lower_bound, upper_bound, draw_pos=True, show_mask=True, threshold=10000)
+
+		comp = bd.conn_comps(mask)[0]
+		# for comp in comps:
+		rect = comp[0]
+		print(f"Rect:{rect}, Length:{comp}")
+		rect_left = (rect[0], rect[1])
+		print(f"Left corner coord is: {rect_left[0]},{rect_left[1]}")
+		rect_right = bd.add(rect_left, (rect[2], rect[3]))
+		print(f"Right corner coord is: {rect_right[0], rect_right[1]}")
+		cv.rectangle(img, rect_left, rect_right, color=(128, 128, 0), thickness=2)
+		roi = img[rect_left[1]:rect_right[1], rect_left[0]:rect_right[0]]
+
+		cv.imshow("cropped_board", roi)
+
+		cv.imshow("find_board", img)
+
+		center, r_image = rd.detect_robot_coords(roi)
+		# tuning_color_filter(resized)
+		cv.imshow('robot', r_image)
+
+		key = cv.waitKey(1)
+		# Выход из программы по нажатию esc
+		if key == 27:
+			exit()
+
+		coord_x, coord_y = center
+		traj_x.append(coord_x)
+		traj_y.append(coord_y)
 
 
+		# The array to be published on the topic.
+		points_info = Points_arrays()
+		points_info.x_coordinates = str(traj_x)
+		points_info.y_coordinates = str(traj_y)
 
 	
-	# The array to be published on the topic.
-	points_info = Points_arrays()
-	points_info.x_coordinates = str(traj_x)
-	points_info.y_coordinates = str(traj_y)
-	
-	#while not rospy.is_shutdown():
-        p_publisher.publish(points_info)
-        #rate.sleep()
+		#while not rospy.is_shutdown():
+        	p_publisher.publish(points_info)
+        	#rate.sleep()
 
-	# spin() simply keeps python from exiting until this node is stopped
-    	rospy.spin()
+		traj_x.clear()
+		traj_y.clear()
+		# spin() simply keeps python from exiting until this node is stopped
+    		rospy.spin()
 
 if __name__ == '__main__':
 	Detector()
